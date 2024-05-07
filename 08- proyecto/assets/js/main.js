ff338f5bc3b23ebe9a8c4ecef89f5920cef4211f
Joaquin <clcflecha@gmail.com>
2024-05-07 08:39:45 -0400

    copia de seguridad

---
+++ 08- proyecto/assets/js/main.js
@@ -0,0 +1,16 @@
+// Enunciado 8:
+// Crea una funcion que devuelva si un numero (enviado por el usuario) 
+// es par o impar.
+
+// Muestra el resultado por la consola, la pantalla y una ventana.
+
+let num = 0;
+let result = 0;
+
+
+do{
+
+    num = parseInt(prompt)
+    
+    
+}while( num <= 0);
