c01ff84c008d763d3df91ee729ecc3559fbc662e
Joaquin <clcflecha@gmail.com>
2024-05-08 14:12:57 -0400

    copia de seguridad

--- 09- proyecto/assets/js/main.js
+++ 09- proyecto/assets/js/main.js
@@ -3,18 +3,26 @@ let result = 0;
 
 function sumAll(...numbers){
 
+    let resultBox = document.querySelector("#result");
+    let numbersBox = document.querySelector("#numbers");
+    
+    numbersBox = "Los numeros que se han utilizado para los resultados son estos: "
+    
     for( let count = 0; count < numbers.length; count++ ){
 
         result += numbers[count];
 
+        numbersBox.innerHTML += numbers[count];
+
+        if( count === numbers.length - 1 ) numbersBox.innerHTML += ', ';
     };
+
     
-    let resultBox = document.querySelector("#result");
-    resultBox.innerHTML = result;
+    resultBox.innerHTML = "El resultado es: " + result;
 
 };
 
-sumAll(3,4,5,6,7,8);
+sumAll(1,2,3,4,5,6);
 
 
 
