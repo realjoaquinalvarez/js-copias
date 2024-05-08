27a003dca09aef0334febb07312e60873ade865d
Joaquin <clcflecha@gmail.com>
2024-05-08 11:45:08 -0400

    copia de seguridad

--- 09- proyecto/assets/js/main.js
+++ 09- proyecto/assets/js/main.js
@@ -1,16 +1,20 @@
 let result = 0;
-let numbers = [0];
 
 
 function sumAll(...numbers){
 
     for( let count = 0; count < numbers.length; count++ ){
 
-        result = numbers[count]
+        result += numbers[count];
 
-    }
+    };
     
-}
+    let resultBox = document.querySelector("#result");
+    resultBox.innerHTML = result;
+
+};
+
+sumAll(3,4,5,6,7,8);
 
 
 
