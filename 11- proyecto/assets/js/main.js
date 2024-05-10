5b246853ef6ed92b31274493f98d7e1d86300cfd
Joaquin <clcflecha@gmail.com>
2024-05-10 08:21:44 -0400

    copia de seguridad

---
+++ 11- proyecto/assets/js/main.js
@@ -0,0 +1,15 @@
+let frase = 'Este curso tiene muchos proyectos';
+
+let palabra = prompt(`En esta frase " ${frase} ". Que palabra quieres reemplazar?`);
+let reemplazo = prompt(`Haz elegido reemplazar la palabra ${ palabra }. Cual es la palabra con la que la quieres sustituir?`);
+
+
+function reemplazar( palabraOriginal, palabraRemplazo){
+
+    let busqueda = frase.search(palabraOriginal);
+
+   
+    
+}
+
+
