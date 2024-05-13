78b57291bd4aac175cce32a0b90885c6f19438a8
Joaquin <clcflecha@gmail.com>
2024-05-13 10:14:37 -0400

    copia de seguridad

--- 12- proyecto/assets/js/main.js
+++ 12- proyecto/assets/js/main.js
@@ -2,3 +2,19 @@
 let animales = ["perro", "gato", "leon", "tigre"]
 
 
+const recorrerMostrar = () => {
+
+    document.write("<h1>Proyecto 12 JS - Joaquin Alvarez</h1>")
+
+    for( let i = 0; i < animales.length; i++ ){
+
+        parrafo = `<p> - ${animales[i]} </p>`;
+
+        document.write(parrafo);
+        
+    }
+    
+}
+
+
+recorrerMostrar();
