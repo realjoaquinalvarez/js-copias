ab7c5e5c7d53c767e37b56a3f1ce131b9b3ede5e
Joaquin <clcflecha@gmail.com>
2024-05-14 17:45:54 -0400

    copia de seguridad

--- 13 - proyecto/assets/js/main.js
+++ 13 - proyecto/assets/js/main.js
@@ -14,9 +14,28 @@ let concesionario = [
 ];
 
 
+function mostrar(datos){
 
+    let cajaConcesionario = document.querySelector('.concesionario')
+    
+    datos.forEach( coches => {
 
+        cajaConcesionario.innerHTML += `<h1> ${ coches.marca } </h1>`;
 
+        cajaConcesionario.innerHTML += `<ul>`;
+        
+        coches.modelos.forEach((modelo) => {
+            cajaConcesionario.innerHTML += `<li> - ${ modelo } </li>`;
+        });
+        
+        cajaConcesionario.innerHTML += `</ul>`;
+        
+    });
+    
+    
+}
+
+mostrar(concesionario);
 
 
 
