c9827058e5723b7b89667da9839cc03e517ee17f
Joaquin <clcflecha@gmail.com>
2024-05-14 10:25:31 -0400

    copia de seguridad

---
+++ 13 - proyecto/assets/js/main.js
@@ -0,0 +1,22 @@
+
+let concecionario = [
+
+    {
+        marca: "mercedes",
+        modelos: ["clase a", "clase b", "clase c"]
+    },
+    {
+        marca: "audi",
+        modelos: ["clase a4", "clase b5", "clase c8"]
+    },
+    {
+        marca: "renault",
+        modelos: ["clio", "capture", "magene"]
+    },
+
+
+    
+    
+]
+
+
