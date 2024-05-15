82cb54e865219ba64922ea2e8e01ad20f90b0a9f
Joaquin <clcflecha@gmail.com>
2024-05-15 10:22:43 -0400

    copia de seguridad

--- 14 - proyecto/assets/js/main.js
+++ 14 - proyecto/assets/js/main.js
@@ -2,14 +2,28 @@ const sueldos = [1050, 640, 750, 1500, 2200, 3011, 432];
 
 let sueldoUsuario = parseInt(prompt("Cual es su sueldo?"));
 
+const listarOrden = (sueldosOrdenados) => {
+
+    let texto = ``
+
+    sueldosOrdenados.forEach(sueldo => {
+        texto += `\n - ${sueldo} `
+    });
+    
+    return texto;
+
+}
+
 const buscarSueldos = (salarios, miSueldo) => {
 
     let busqueda = salarios.filter( salario => salario >= miSueldo );
 
     let ordenar = busqueda.sort(( a, b ) =>  a - b );
 
-    console.log(`Hay ${ ordenar.length} salarios mayores al tuyo (${miSueldo})$, son estos: ${ordenar}`);
+    console.log(`Hay ${ ordenar.length} salarios mayores al tuyo (${miSueldo})$, son estos: ${listarOrden(ordenar)}`);
     
 }
+
+
 
 buscarSueldos(sueldos, sueldoUsuario);
