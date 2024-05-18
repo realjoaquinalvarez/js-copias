de6defd1b86d1e6339ffc681336360bb6d7e8cc9
Joaquin <clcflecha@gmail.com>
2024-05-18 13:12:09 -0400

    copia de seguridad

--- 16 - proyecto/assets/js/main.js
+++ 16 - proyecto/assets/js/main.js
@@ -2,7 +2,9 @@
 console.log(screen.width, screen.height, window.location.href )
 let data = document.querySelector('.data');
 
-data.innerHTML += `<h2>Anchura: ${screen.width}</h2>`;
-data.innerHTML += `<h2>Altura: ${screen.height}</h2>`;
+data.innerHTML += `<h2>Anchura: ${window.innerWidth}</h2>`;
+data.innerHTML += `<h2>Altura: ${window.innerHeight}</h2>`;
 data.innerHTML += `<h2>URL: ${window.location.href}</h2>`;
+
+window.open("https://victorrobles.es/ruta", "blank");
 
