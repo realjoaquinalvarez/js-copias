658821afdbc834bb0ff753b8f1df918d21a5db5c
Joaquin <clcflecha@gmail.com>
2024-05-20 15:32:41 -0400

    copia de seguridad

---
+++ 18 - proyecto/assets/js/main.js
@@ -0,0 +1,16 @@
+// Contadores de minutos y segundos
+let cronoMin = document.querySelector('.crono__min');
+let cronoSeg = document.querySelector('.crono__sec');
+
+// Variables seleccion botones
+let btnStart = document.querySelector('.layout__btn--start');
+let btnStop = document.querySelector('.layout__btn--stop');
+
+// Variables cuenta minutos y segundos
+let minutes = 0;
+let seconds = 0;
+
+// Funcion para iniciar cronometro
+
+
+
