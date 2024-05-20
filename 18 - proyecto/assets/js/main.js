56d51c94a9e39b877341fe91ca8e2395fcd60f8a
Joaquin <clcflecha@gmail.com>
2024-05-20 19:20:25 -0400

    copia de seguridad

--- 18 - proyecto/assets/js/main.js
+++ 18 - proyecto/assets/js/main.js
@@ -9,25 +9,51 @@ let btnStop = document.querySelector('.layout__btn--stop');
 // Variables cuenta minutos y segundos
 let minutes = 0;
 let seconds = 0;
+let time = null;
 
 // Funcion para iniciar cronometro
 let start = () => {
-    
-    setInterval(() => {
-        seconds ++;
+    if(!time) {
+        time = setInterval(() => {
+            seconds ++;
+            if(seconds < 10) {
+                cronoSeg.innerHTML = '0' + seconds;
+            } else {
+                cronoSeg.innerHTML = seconds;
+            }
+                        
+            if( seconds == 60 ){
+                minutes++
+                seconds = 0;
+                if( minutes < 10 ){
+                    cronoMin.innerHTML = '0' + minutes;
+                } else {
+                    cronoMin.innerHTML = minutes;
+                }
+            };
+            
 
-        
-        if( seconds == 60 ){
-            minutes++
-            seconds = 0;
-        };
-        
-        console.log(`Es el minuto ${minutes }, segundo ${seconds}`)
-
-    }, 1000);
+            if( minutes >= 60 ){
+                clearInterval(time)
+                minutes = 0;
+                seconds = 0;
+                cronoSeg.innerHTML = '0' + seconds;
+                cronoMin.innerHTML = '0' + minutes ;
+                alert('Haz excedido el tiempo limite de 60 minutos');
+                return;
+            }
+            
+            
+            
+        }, 1000);
+    }
     
 };
 
 btnStart.addEventListener("click", () => {
     start();
+})
+
+btnStop.addEventListener("click", () => {
+    minutes = 60;
 })
