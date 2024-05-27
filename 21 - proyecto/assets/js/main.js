b08c9453bc7ed6d2e357f514fe62134cd20bdd8c
Joaquin <clcflecha@gmail.com>
2024-05-27 17:55:11 -0400

    copia de seguridad

--- 21 - proyecto/assets/js/main.js
+++ 21 - proyecto/assets/js/main.js
@@ -9,17 +9,50 @@ let estanteriaDom = document.querySelectorAll(".almacen__estanteria");
 
 // Poner un id unico a cada caja movible
 
-cajas.forEach((caja, indice) => {
+cajas.forEach((caja, i) => {
 
     caja.setAttribute("id", "caja"+i);
 
     caja.addEventListener("dragstart", (e) => {
-        e.dataTransfer.setData("id", e.target.id);
+        e.dataTransfer.setData("dragId", e.target.id);
     })
     
 });
 
 // Establecer cada hueco de la estanteria como un hueco donde puedo soltar un elemento
+
+
+estanteriaDom.forEach((estanteria, i) => {
+
+    estanteria.addEventListener("dragover", e => {
+        e.preventDefault();        
+    });
+    
+    estanteria.addEventListener("drop", e => {
+        e.preventDefault();
+        
+        let idCajaDrag = e.dataTransfer.getData("dragId");
+
+        if(idCajaDrag){
+
+            let cajaNodo = document.querySelector("#" + idCajaDrag);
+
+            if(e.target.childElementCount === 0){
+                e.target.appendChild(cajaNodo);
+                e.target.style.boxShadow = "none";
+            } else {
+                alert("esta estanteria esta ocupada!!");
+            }
+
+        }
+        
+        
+    });
+    
+    
+});
+
+
 
 
 
