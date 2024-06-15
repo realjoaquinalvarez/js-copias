1bdbf54df0e45f8774113ce7aa90f7dad6ddf080
Joaquin <clcflecha@gmail.com>
2024-06-15 06:14:23 -0400

    copia de seguridad

--- 40 - proyecto/assets/js/main.js
+++ 40 - proyecto/assets/js/main.js
@@ -1,14 +1,48 @@
 
 let stars = document.querySelectorAll('.star');
 
+let starCounter = 0;
+let starStorage = 0;
+
+stars.forEach(star => {
+   starCounter++;
+   let starNumber = starCounter;
+
+   star.addEventListener("mouseover", () => {
+      marcarEstrella( starNumber );
+   });
+
+   star.addEventListener('click', () => {
+      starStorage = starNumber;
+   });
+   
+   star.addEventListener("mouseout", () => {
+      marcarEstrella( starStorage );
+   });
+
+});
         
+const marcarEstrella = ( calificacion = 0 ) => {
+
+   stars.forEach((star, starPosition) => {
+
+      if( starPosition < calificacion){
+         star.classList.add('active');
+      }else{
+         star.classList.remove('active');
+      }
+      
+   });
+   
+};
         
-        
-        
-        
-        
-        
-        
-        
-        
+const desmarcarEstrella = () => {
+
+   stars.forEach((star) => {
+
+      star.classList.remove('active');
+      
+   });
+   
+};
         
