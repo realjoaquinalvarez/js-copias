30227ef02093272011f789e9727ffeb617d220d6
Joaquin <clcflecha@gmail.com>
2024-06-15 15:32:37 -0400

    copia de seguridad

--- 41 - proyecto/assets/js/main.js
+++ 41 - proyecto/assets/js/main.js
@@ -1,10 +1,29 @@
 const articles = document.querySelectorAll('.noticia');
-console.log(articles);
 
 const observer = new IntersectionObserver((entries) => {
-  console.log(entries);
+
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("visible")
+        } else {
+            entry.target.classList.remove("visible")
+        }
+    });
+  
 });
 
 articles.forEach((article) => {
   observer.observe(article);
 });
+
+
+
+
+
+
+
+
+
+
+
+
