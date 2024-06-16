0a2dbec0f4041b65b6354b45b72be4a4a71eac98
Joaquin <clcflecha@gmail.com>
2024-06-16 15:30:11 -0400

    copia de seguridad

--- 46 - proyecto/script.js
+++ 46 - proyecto/script.js
@@ -15,7 +15,6 @@ function hideAllContents() {
     contents.forEach(content => content.classList.remove('show'))
 }
 
-
 function hideAllItems() {
     listItems.forEach(item => item.classList.remove('active'))
 }
